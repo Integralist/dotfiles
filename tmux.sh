4057d5203df81909b6f495be0bbbd4f3605f697d
Mark McDonnell <Integralist@users.noreply.github.com>
2018-11-14 07:58:51 +0000

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -26,14 +26,6 @@ else
   tmux new-window -n 'ipython' -t work:3
   tmux send-keys -t work:3 'cd ~/code/python3.6.3 && ipython' 'C-m'
 
-  # CDN
-  tmux new-window -n 'cdn' -t work:4
-  tmux send-keys -t work:4 'cd ~/code/buzzfeed/cdn' 'C-m'
-
-  # Go Code Project
-  tmux new-window -n 'go' -t work:5
-  tmux send-keys -t work:5 'cd ~/code/go' 'C-m'
-
   # Attach to the session, now we have everything setup
   # We connect to the Rig window as we have to manually run vagrant (due to GPG passphrase requirement)
   tmux attach -t work:2
