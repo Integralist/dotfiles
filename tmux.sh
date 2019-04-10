b8e710bd3ddf3f49013af23c5c272b3882cc6de4
Mark McDonnell <Integralist@users.noreply.github.com>
2019-04-10 16:15:41 +0100

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -8,25 +8,23 @@ else
   # I then wonder why I can't push to GitHub/GitLab
   eval "$(ssh-agent -s)"
   ssh-add -K ~/.ssh/github_rsa
-  ssh-add -K ~/.ssh/gitlab_rsa
 
   # Create a new session and detach from it
   tmux new -s work -d
 
   # Jump into our BuzzFeed directory
   tmux rename-window -t 1 'mono'
-  tmux send-keys -t work:1 'cd ~/code/buzzfeed/mono' 'C-m'
+  tmux send-keys -t work:1 'cd ~/Code/buzzfeed/mono' 'C-m'
 
   # Prepare Rig VM
   tmux new-window -n 'rig' -t work:2
   tmux send-keys -t work:2 'export GPG_USER=mark.mcdonnell@buzzfeed.com' 'C-m'
-  tmux send-keys -t work:2 '# "execute v up followed by sshvm command (requires gpg passphrase)"' 'C-m'
+  tmux send-keys -t work:2 'cd ~/Code/buzzfeed/mono && python3 -m pip install -e ./rig && rig local bootstrap' 'C-m'
 
   # Pipenv Python REPL
   tmux new-window -n 'pipenv 3.7' -t work:3
-  tmux send-keys -t work:3 'cd ~/code/python3.7 && pipenv run ipython' 'C-m'
+  tmux send-keys -t work:3 'cd ~/Code/Python/3.7.3 && pipenv run ipython' 'C-m'
 
-  # Attach to the session, now we have everything setup
-  # We connect to the Rig window as we have to manually run vagrant (due to GPG passphrase requirement)
-  tmux attach -t work:2
+  # Now everything is setup we'll attach to a specific window we're interested in
+  tmux attach -t work:3
 fi
