c800d97ebcad0f181689e096addef6fcd7c9fe35
Integralist <mark.mcdx@gmail.com>
2020-05-14 14:01:17 +0100

    update tmux

--- tmux.sh
+++ tmux.sh
@@ -12,23 +12,21 @@ else
   # Create a new session and detach from it
   tmux new -s work -d
 
-  # Jump into our BuzzFeed directory
-  tmux rename-window -t 1 'mono'
+  # BuzzFeed
+  tmux rename-window -t 1 'buzzfeed'
   tmux send-keys -t work:1 'cd ~/Code/buzzfeed/mono' 'C-m'
-
-  # Prepare Rig VM
-  tmux new-window -n 'rig' -t work:2
-  tmux send-keys -t work:2 'export GPG_USER=mark.mcdonnell@buzzfeed.com' 'C-m'
-  tmux send-keys -t work:2 'cd ~/Code/buzzfeed/mono && pyenv activate bfmono && python3 -m pip install -e ./rig && rig local bootstrap' 'C-m'
+  tmux split-window -p 30
+  tmux send-keys -t work:1 'export GPG_USER=mark.mcdonnell@buzzfeed.com' 'C-m'
+  tmux send-keys -t work:1 'cd ~/Code/buzzfeed/mono && pyenv activate bfmono && python3 -m pip install -e ./rig && rig local bootstrap' 'C-m'
 
   # ipython REPL
-  tmux new-window -n 'ipython' -t work:3
-  tmux send-keys -t work:3 'cd ~/Code/Python/3.8-dev && pyenv activate ipython_env && ipython' 'C-m'
+  tmux new-window -n 'ipython' -t work:2
+  tmux send-keys -t work:2 'cd ~/Code/Python/3.8-dev && pyenv activate ipython_env && ipython' 'C-m'
 
   # htop
-  tmux new-window -n 'htop' -t work:4
-  tmux send-keys -t work:4 'htop' 'C-m'
+  tmux new-window -n 'htop' -t work:3
+  tmux send-keys -t work:3 'htop' 'C-m'
 
   # Now everything is setup we'll attach to a specific window we're interested in
-  tmux attach -t work:4
+  tmux attach -t work:3
 fi
