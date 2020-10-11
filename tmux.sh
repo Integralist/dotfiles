ab8fc5c8bda0b1d0b239aa98a79d95bb0db81b61
Mark McDonnell <Integralist@users.noreply.github.com>
2020-10-11 19:03:46 +0100

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
 
-# Check if we've not already got a session created
+# check if we've not already got a session created
+#
 if [[ $(tmux ls 2> /dev/null | grep -c work) -eq 1 ]]; then
   tmux attach -t work
 else
-  # Every time I restart my computer I forget about doing this
-  # I then wonder why I can't push to GitHub/GitLab
-  eval "$(ssh-agent -s)"
-  ssh-add -K ~/.ssh/github_rsa
-
-  # Create a new session and detach from it
+  # create a new session and detach from it
+  #
   tmux new -s work -d
 
-  # BuzzFeed
-  tmux rename-window -t 1 'buzzfeed'
-  tmux send-keys -t work:1 'cd ~/Code/buzzfeed/mono' 'C-m'
+  # work stuff
+  #
+  tmux rename-window -t 1 'fastly'
+  tmux send-keys -t work:1 'cd ~/Code/fastly' 'C-m'
   tmux split-window -p 30
-  tmux send-keys -t work:1 'export GPG_USER=mark.mcdonnell@buzzfeed.com' 'C-m'
-  tmux send-keys -t work:1 'cd ~/Code/buzzfeed/mono && pyenv activate bfmono && python3 -m pip install -e ./rig && rig local bootstrap' 'C-m'
+  tmux send-keys -t work:1 'export GPG_USER=mark.mcdx@gmail.com' 'C-m'
 
   # ipython REPL
+  # expects a 'repl' virtual environment to have already been created
+  #
   tmux new-window -n 'ipython' -t work:2
-  tmux send-keys -t work:2 'cd ~/Code/Python/3.8-dev && pyenv activate ipython_env && ipython' 'C-m'
+  tmux send-keys -t work:2 'cd ~/Code/python && pyenv activate repl && ipython' 'C-m'
 
-  # htop
-  tmux new-window -n 'htop' -t work:3
-  tmux send-keys -t work:3 'htop' 'C-m'
-
-  # Now everything is setup we'll attach to a specific window we're interested in
-  tmux attach -t work:3
+  # now everything is setup we'll attach to a specific window we're interested in
+  #
+  tmux attach -t work:2
 fi
