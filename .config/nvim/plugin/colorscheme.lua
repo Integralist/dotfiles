e80d84a850f66c6b90443ce6782b22730812f46b
Integralist <mark.mcdx@gmail.com>
2022-07-29 17:39:24 +0100

    update colourscheme setup

---
+++ .config/nvim/plugin/colorscheme.lua
@@ -0,0 +1,6 @@
+-- We remove the Vim builtin colorschemes so they don't show in Telescope.
+vim.cmd([[
+  silent !rm $VIMRUNTIME/colors/*.vim &> /dev/null
+  set background=dark
+  colorscheme gruvbox
+]])
