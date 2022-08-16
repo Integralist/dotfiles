5d4f68848654e95f67a98eb988dbb085c83ea9f4
Integralist <mark.mcdx@gmail.com>
2022-08-16 09:28:48 +0100

    add window picker to neovim

--- .config/nvim/lua/plugin-manager.lua
+++ .config/nvim/lua/plugin-manager.lua
@@ -178,6 +178,21 @@ return require("packer").startup({
       end
     }
 
+    -- window picker
+    use { "s1n7ax/nvim-window-picker", tag = "v1.*",
+      config = function()
+        local picker = require("window-picker")
+        picker.setup({
+          fg_color = "#000000",
+        })
+
+        vim.keymap.set("n", "<leader><leader>w", function()
+          local picked_window_id = picker.pick_window() or vim.api.nvim_get_current_win()
+          vim.api.nvim_set_current_win(picked_window_id)
+        end, { desc = "Pick a window" })
+      end,
+    }
+
     -- whitespace management
     use { "zakharykaplan/nvim-retrail",
       config = function()
