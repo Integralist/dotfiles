40500abd89beabc75a8f5b7e75afe8899a7c1623
Integralist <mark.mcdx@gmail.com>
2022-08-26 10:59:25 +0100

    fix colorcolumn

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -27,21 +27,16 @@ vim.o.tabstop = 2
 vim.o.updatetime = 1000 -- affects CursorHold and subsequently things like highlighting Code Actions.
 vim.o.wrap = false
 
-vim.cmd([[ set colorcolumn=80 ]])
-
---[[
-https://www.reddit.com/r/neovim/comments/wy4pfo/how_to_set_colorcolumn_via_nvim_lua_api/
-
-vim.api.nvim_set_option("colorcolumn", "80")
-
-local opts = {}
-opts["scope"] = "global"
-vim.api.nvim_set_option_value("colorcolumn", "80", opts)
-]]
-
 if vim.fn.has("termguicolors") == 1 then
   vim.o.termguicolors = true
 end
+
+--[[
+vim.o allows you to set global vim options, but not local buffer vim options.
+vim.opt has a more expansive API that can handle local and global vim options.
+See :h lua-vim-options
+]]
+vim.opt.colorcolumn = "80"
 
 -- LSP configuration options
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
