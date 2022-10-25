1359f1792a2877104bfc5b52622d582475cde513
Integralist <mark.mcdx@gmail.com>
2022-10-25 19:40:03 +0100

    update snippet implementation

--- .snippets/go.lua
+++ .snippets/go.lua
@@ -3,6 +3,17 @@
 --
 -- GLOBAL VARS:
 -- https://github.com/L3MON4D3/LuaSnip/blob/69cb81cf7490666890545fef905d31a414edc15b/lua/luasnip/config.lua#L82-L104§
+--
+-- EXAMPLES:
+-- https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/go.json
+
+local session = require("luasnip.session")
+
+local env = session.config.snip_env
+local s = env["s"]
+local t = env["t"]
+local i = env["i"]
+local parse = env["parse"]
 
 return {
   s({ trig = "co", name = "Constant", dscr = "Insert a constant" }, {
