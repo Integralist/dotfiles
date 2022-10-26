7b941208206aa9a1b17e2442eea0aeb1bfa52f4d
Integralist <mark.mcdx@gmail.com>
2022-10-26 19:42:02 +0100

    update go snippets

--- .snippets/go.lua
+++ .snippets/go.lua
@@ -17,20 +17,24 @@ local parse = env["parse"]
 
 return {
   s({ trig = "co", name = "Constant", dscr = "Insert a constant" }, {
-    t "const ", i(1, "name"), t " = ", i(2, "value")
+    t("const "), i(1, "name"), t(" = "), i(2, "value")
   }),
 
-  parse("iferr", [[
+  s({ trig = "pf", name = "Formatted Print", dscr = "Insert a formatted print statement" }, {
+    t("fmt.Printf(\"%+v\\n\", "), i(1, "value"), t(")")
+  }),
+
+  parse({ trig = "ife", name = "If Err", dscr = "Insert a basic if err not nil statement" }, [[
   if err != nil {
     return err
   }
   ]]),
 
-  parse("iferrwrap", [[
-  if err != nil {
-    return fmt.Errorf("error: %w", err)
-  }
-  ]]),
-
-  parse("pfmt", "fmt.Printf(\"%+v\\n\", )"),
+  s({ trig = "ifew", name = "If Err Wrapped", dscr = "Insert a if err not nil statement with wrapped error" }, {
+    t("if err != nil {"),
+    t({ "", "  return fmt.Errorf(\"failed to " }),
+    i(1, "message"),
+    t(": %w\", err)"),
+    t({ "", "}" })
+  }),
 }
