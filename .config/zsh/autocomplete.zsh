acd868390b365d814b0f5155faec2fa4a1ee78b7
Integralist <mmcdonnell@fastly.com>
2025-01-27 15:43:22 +0000

    update zsh

--- .config/zsh/autocomplete.zsh
+++ .config/zsh/autocomplete.zsh
@@ -49,26 +49,14 @@ fi
 #
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
-# set descriptions format to enable group support
-# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
-zstyle ':completion:*:descriptions' format '[%d]'
-# set list-colors to enable filename colorizing
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
 # preview directory's content with eza when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-# custom fzf flags
-# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+# custom fzf flags as it doesn't follow FZF_DEFAULT_OPTS by default
 zstyle ':fzf-tab:*' fzf-flags --style=full --color=fg:1,fg+:2 --bind=tab:accept
 # switch group using `<` and `>`
 zstyle ':fzf-tab:*' switch-group '<' '>'
-
-# DISABLED: As the preview window was annoying.
-#
-# # To make fzf-tab follow FZF_DEFAULT_OPTS.
-# # NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
-# zstyle ':fzf-tab:*' use-fzf-default-opts yes
 
 # Git Autocomplete
 #
